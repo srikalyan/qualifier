b56a03fc98fcaa0828d6c3e3633058716999d7cf
Srikalyan Swayampakula <srikalyansswayam@gmail.com>
2019-08-10 23:02:02 -0700

    Added long description.

--- setup.py
+++ setup.py
@@ -3,10 +3,15 @@ from setuptools import setup, find_packages
 # PLEASE DO NOT EDIT THIS, MANAGED FOR CI PURPOSES
 __QUALIFIER__ = ""
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(
     name="qualifier",
-    version="1.0.0" + __QUALIFIER__,
+    version="1.1.0" + __QUALIFIER__,
     description="A simple python project used for updating the qualifier part of the version for python projects.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     author="Srikalyan Swayampakula",
     author_email="srikalyansswayam@gmail.com",
     url="https://github.com/srikalyan/qualifier",
